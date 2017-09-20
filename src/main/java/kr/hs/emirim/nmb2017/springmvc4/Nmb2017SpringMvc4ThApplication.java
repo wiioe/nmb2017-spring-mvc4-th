345a6a5c40d03e1139ac2ee3dc9d2cde899a7a76
Hyunju Jeong <0106grace@naver.com>
2017-09-20 10:36:01 +0900

    twitter 2번째 예제

---
+++ src/main/java/kr/hs/emirim/nmb2017/springmvc4/Nmb2017SpringMvc4ThApplication.java
@@ -0,0 +1,12 @@
+package kr.hs.emirim.nmb2017.springmvc4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nmb2017SpringMvc4ThApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nmb2017SpringMvc4ThApplication.class, args);
+	}
+}
